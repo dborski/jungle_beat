13ae1478918ea9f46133dff282b06801dc1b1fb6
Derek Borski <derek.borski@gmail.com>
2020-06-25 15:58:46 -0600

    Initialize Node class. Methods and tests for data and next_node methods

---
+++ test/node_test.rb
@@ -0,0 +1,22 @@
+require_relative 'test_helper'
+require './lib/node'
+
+class NodeTest < Minitest::Test
+
+  def setup
+    @node = Node.new("plop")
+  end
+
+  def test_it_exists
+    assert_instance_of Node, @node
+  end
+
+  def test_it_has_readable_attributes
+    assert_equal "plop", @node.data
+  end
+
+  def test_next_node_is_nil
+    assert_equal nil, @node.next_node
+  end
+end 
+
